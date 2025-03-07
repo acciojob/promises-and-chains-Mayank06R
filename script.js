e79b6f04e4ec72efea2199fa740949b800c84c7b
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-03-07 20:00:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const ageForm = document.getElementById('ageForm');
+
+    ageForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const age = document.getElementById('age').value;
+        const name = document.getElementById('name').value;
+
+        if (age === "" || name === "") {
+            alert("Please enter valid details.");
+            return;
+        }
+
+        new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                if (age > 18) {
+                    resolve(`Welcome, ${name}. You can vote.`);
+                } else {
+                    reject(`Oh sorry ${name}. You aren't old enough.`);
+                }
+            }, 4000);
+        })
+        .then(function (message) {
+            alert(message);
+        })
+        .catch(function (message) {
+            alert(message);
+        });
+    });
+});
